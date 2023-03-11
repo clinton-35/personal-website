08a08df722660b1b99c9c72db3db1bc03e095d42
clinton <clinton.wambua@student.moringaschool.com>
2023-03-11 08:14:01 +0300

    website complete

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("button").onclick= function(){
+    document.getElementById("confirm").innerHTML = "Thank you";
+    document.getElementById("button").style.display = "none";
+}
